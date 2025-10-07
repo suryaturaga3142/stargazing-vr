be7a3ee9ea184da7eff3550bd70c241be2641381
mzavada162 <mzavada@purdue.edu>
2025-10-07 15:20:30 -0400

    Created star_coords_to_quaternion()
    Created quaternion struct, and a function that takes in the Right Ascension (RA) and Declination (Dec) angles of a star, and outputs the quaternion corresponding to that star's location

---
+++ star_movement.c
@@ -0,0 +1,24 @@
+/* 
+ * Functions for modeling Earth's rotation and how it affects star location
+*/
+
+#include <math.h>
+
+struct quaternion {
+    float real;
+    float i;
+    float j;
+    float k;
+};
+
+//Take in RA and Dec angles in radians of a star and output that star's quaternion location
+struct quaternion star_coords_to_quaternion(float angle_ra_rad, float angle_dec_rad)
+{
+    struct quaternion q;
+    q.real = 0;
+    q.i = math.cos(angle_dec_rad) * math.cos(angle_ra_rad);
+    q.j = math.cos(angle_dec_rad) * math.sin(angle_ra_rad);
+    q.k = math.sin(angle_dec_rad);
+    return q;
+}
+
