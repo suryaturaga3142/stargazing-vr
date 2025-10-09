7377a296f23f2658a12b35eec1378c3dfd6db3c8
mzavada162 <mzavada@purdue.edu>
2025-10-09 10:05:15 -0400

    Wrote outline of functions to rotate Earth and update star locations based on that rotation

--- star_movement.c
+++ star_movement.c
@@ -11,14 +11,17 @@ struct quaternion {
     float k;
 };
 
-//Take in RA and Dec angles in radians of a star and output that star's quaternion location
-struct quaternion star_coords_to_quaternion(float angle_ra_rad, float angle_dec_rad)
+static float earth_angular_velocity = (7.292115e-5 * 180 / math.pi); //degrees per second
+float[] earth_axis; //TODO: Make quaternion instead so it's easier to update based on position relative to GPS location?
+
+//Take in RA and Dec angles in degrees for a star and output that star's quaternion location
+struct quaternion star_coords_to_quaternion(float angle_ra_deg, float angle_dec_deg)
 {
     struct quaternion q;
     q.real = 0;
-    q.i = math.cos(angle_dec_rad) * math.cos(angle_ra_rad);
-    q.j = math.cos(angle_dec_rad) * math.sin(angle_ra_rad);
-    q.k = math.sin(angle_dec_rad);
+    q.i = math.cos(angle_dec_deg) * math.cos(angle_ra_deg);
+    q.j = math.cos(angle_dec_deg) * math.sin(angle_ra_deg);
+    q.k = math.sin(angle_dec_deg);
     return q;
 }
 
@@ -31,6 +34,27 @@ struct quaternion rotation_to_quaternion(float angle, float[] axis)
     q_rotation.i = math.sin(angle / 2) * axis[0];
     q_rotation.j = math.sin(angle / 2) * axis[1];
     q_rotation.k = math.sin(angle / 2) * axis[2];
+    return q_rotation;
+}
+
+//Get the quaternion for the rotation of the earth based on the amount of time passed
+//TODO: Is there a way to have a function as the quaternion angle? Or is this good?
+struct quaternion get_earth_rotation(float seconds)
+{
+    float angle = (seconds * earth_angular_velocity) % 360;
+    struct quaternion earth_rotation = rotation_to_quaternion(angle, earth_axis);
+    return earth_rotation;
+}
+
+//Update star positions based on Earth rotating for a set period of time
+//TODO: Finish function
+void rotate_earth(float seconds)
+{
+    struct quaternion earth_rotation = get_earth_rotation(seconds);
+    struct quaternion new_star;
+    // for each star in list of stars
+    //      new_star = rotate_quaternion(star, earth_rotation); 
+    //      replace star in list with new star value
 }
 
 //Take the conjugate of a quaternion
@@ -73,8 +97,20 @@ float convert_UT_to_GMST(int D, float H)
 float convert_GMST_to_LST(float GMST, float longitude)
 {
     float LST; //Local Standard Time
-    //TODO: Check this conversion, not sure if the 15 degrees per hour still applies in sidereal time
-    float longitude_hours = longitude / 15; //Convert longitude to hours, 15 degrees per hour
+    float longitude_hours = longitude / 15.041; //Convert longitude to hours, 15.041 degrees per hour
     LST = (GMST + longitude_hours) % 24; 
     return LST;
+}
+
+//Get user's starting location and time and initialize stars based on that info
+//TODO: Finish function
+void init_star_locations()
+{
+    //TODO: Convert all stars to quaternions from list of RA and Dec angles using star_coords_to_quaternion() fxn
+    float longitude, latitude; //TODO: Call function to get location from GPS
+    int days; //TODO: Call function to get date and time
+    float hours; 
+    float rotation_seconds = 3600 * convert_GMST_to_LST(convert_UT_to_GMST(days, hours), longitude); //Get time difference from 
+    rotate_earth(rotation_seconds); //Rotate Earth the necessary amount of time to get star locations relative to user's longitude
+    return;
 }
