4df704afbccc1e62229e38bcf97552bc742b5899
sandeep saravanakumar <sarava16@purdue.edu>
2025-10-16 22:29:36 -0400

    uart comms done

--- include/drivers/gps.h
+++ include/drivers/gps.h
@@ -13,6 +13,23 @@
 #ifndef GPS_H
 #define GPS_H
 
+#include "pico/stdlib.h"
+#include "hardware/uart.h"
+
+typedef struct {
+    char time[11];
+    char status;
+    double latitude;
+    double longitude;
+    char date[7];
+} gps_data_t; 
+
+void init_uart(uint baud_rate);
+void uart_send(const uint8_t *data, size_t length);
+int uart_read(char *buffer, size_t max_len);
+
+void gps_init(void);
+int gps_read_data(gps_data_t *data);
 
 
 #endif /* GPS_H */
--- src/drivers/gps.c
+++ src/drivers/gps.c
@@ -14,10 +14,12 @@
 
 /* ----------------------------- Private Includes --------------------------- */
 #include "gps.h"
-// ...
+#include "string.h"
 
 /* ---------------------------- Private Constants --------------------------- */
-// ...
+#define UART_ID uart0
+#define TX_PIN 0
+#define RX_PIN 1
 
 /* ----------------------------- Private Variables -------------------------- */
 // ...
@@ -26,4 +28,35 @@
 // ...
 
 /* ----------------------------- Public Functions --------------------------- */
+void init_uart(uint baud_rate)
+{ 
+    gpio_set_function(TX_PIN, GPIO_FUNC_UART);
+    gpio_set_function(RX_PIN, GPIO_FUNC_UART);
+    uart_init(UART_ID, baud_rate);
+    uart_set_format(UART_ID, 8, 1, UART_PARITY_NONE);
+}
 
+void uart_send(const uint8_t *data, size_t length) 
+{
+    uart_write_blocking(UART_ID, data, length);
+}
+
+int uart_read(char *buffer, size_t max_len)
+{
+    int i;
+    for(i = 0; i < max_len-1; i++)
+    {
+        while(!uart_is_readable(UART_ID))
+        {
+            tight_loop_contents();
+        }
+        char c = uart_getc(UART_ID);
+        buffer[i] = c;
+        if(c == '\n')
+        {
+            break;
+        }
+    }
+    buffer[i+1] = '\0';
+    return i+1;
+}
