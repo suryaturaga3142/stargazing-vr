070961fb3947c0b1b5df9db07d06812224968394
Surya Turaga <suryapratheek.turaga@gmail.com>
2025-10-24 16:53:08 -0400

    cleared

--- src/main.c
+++ src/main.c
@@ -22,7 +22,7 @@
 #include "pico/stdlib.h"
 
 /* ---------------------------- Private Constants --------------------------- */
-#define GPS_TEST
+#define GPS_RAW_TEST
 // #define IMU_TEST
 // #define LCD_SPI_TEST
 // ...
@@ -35,54 +35,17 @@
 
 /* ----------------------------- Public Functions --------------------------- */
 
-#ifdef GPS_TEST
-
-// How often to print new data (in milliseconds)
-#define PRINT_INTERVAL_MS 1000
+#ifdef GPS_RAW_TEST
 
 int main() {
-    // Initialize stdio for printf over USB
-    stdio_init_all();
-    sleep_ms(2000); // Wait for terminal to connect
-    printf("Pico SDK Peripheral Sandbox Started...\n");
-
-    // Initialize the GPS module
-    gps_init();
-    printf("GPS module initialized. Waiting for data...\n");
-
-    // Timer for periodic printing
-    absolute_time_t next_print_time = make_timeout_time_ms(PRINT_INTERVAL_MS);
-
-    while (true) {
-        // Continuously poll the GPS driver to process new NMEA sentences
-        gps_update();
-
-        // Periodically print the latest known data
-        if (time_reached(next_print_time)) {
-            next_print_time = make_timeout_time_ms(PRINT_INTERVAL_MS); // Reset timer
-            
-            gps_data_t data = gps_get_data();
-
-            if (data.fix_valid) {
-                printf("[GPS_TEST] Fix: VALID, Sats: %d\n", data.satellites_tracked);
-                printf("  Lat: %f, Lon: %f\n", data.latitude, data.longitude);
-                printf("  Alt: %f m, Speed: %f knots\n", data.altitude, data.speed);
-            } else {
-                printf("[GPS_TEST] Fix: INVALID (Searching...)\n");
-            }
-        }
-        
-        // You could add other tasks here.
-        // We don't sleep, so gps_update() is polled as fast as possible.
-        // tight_loop_contents(); // Use this if you have no other tasks
-    }
 
     return 0;
 }
 
 #endif
 
-#ifdef IMU_H
+
+#ifdef IMU_TEST
 
 int main() {
 
@@ -93,5 +56,10 @@ int main() {
 
 
 #ifdef LCD_SPI_TEST
+
+int main() {
+
+    return 0;
+}
 
 #endif
