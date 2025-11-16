2f72f6e3a3b73333ad33d02a63d3df8f2c865444
sandeep saravanakumar <sarava16@purdue.edu>
2025-11-16 17:23:35 -0500

    Add FatFS as normal folder

---
+++ lib/no-OS-FatFS-SD-SDIO-SPI-RPi-Pico/include/FatFsSd_C.h
@@ -0,0 +1,24 @@
+/* FatFsSd_C.h
+Copyright 2023 Carl John Kugler III
+
+Licensed under the Apache License, Version 2.0 (the License); you may not use 
+this file except in compliance with the License. You may obtain a copy of the 
+License at
+
+   http://www.apache.org/licenses/LICENSE-2.0 
+Unless required by applicable law or agreed to in writing, software distributed 
+under the License is distributed on an AS IS BASIS, WITHOUT WARRANTIES OR 
+CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+specific language governing permissions and limitations under the License.
+*/
+#pragma once
+
+#include "ff.h"
+//
+#include "diskio.h" /* Declarations of disk functions */
+#include "f_util.h"
+#include "my_rtc.h"
+#include "sd_card.h"
+#include "rp2040_sdio.h"
+#include "my_spi.h"
+#include "hw_config.h"
